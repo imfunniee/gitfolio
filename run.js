c36cd185588437cceacbd3acbe23fc7a5aa97067
Jens Reidel <jens@troet.org>
2019-07-04 09:52:52 +0200

    fix run command

--- run.js
+++ run.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const open = require('open');
-const defaultBrowser = require('x-default-browser');
 const path = require('path');
 const outDir = path.resolve('./dist/' || process.env.OUT_DIR);
 const app = express();
@@ -11,14 +9,9 @@ function runCommand(){
     res.sendFile('/index.html');
   });
 
-  app.listen(3000);
-
-  defaultBrowser(function (err, res) {
-      if(err) throw err;
-      (async () => {
-          await open('http://localhost:3000', {app: res.commonName});
-          console.log("ctrl + c to exit");
-      })();
+  app.listen(3000, function() {
+    console.log('gitfolio running on port 3000');
+    console.log('ctrl + c to exit');
   });
 }
 
