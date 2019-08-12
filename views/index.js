4ded27c56f11e94509c5ef09edd25411bbdec6c1
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-08-12 11:17:01 +0530

    Delete index.js

--- views/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#socials").addEventListener("change", event => {
-  if (event.target.checked) {
-    document.querySelector("#input_for_socials").style.display = "block";
-  } else {
-    document.querySelector("#input_for_socials").style.display = "none";
-  }
-});
